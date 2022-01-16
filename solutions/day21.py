315e848961cfcded87be2119dcfe130779b97d37
nitekat1124 <nitekat1124@gmail.com>
2022-01-16 21:29:06 +0800

    Day 21, reformat the stuffs

--- solutions/day21.py
+++ solutions/day21.py
@@ -60,105 +60,29 @@ class Solution(SolutionBase):
             "armor": int(data[2].split(": ")[1]),
         }
         weapons = {
-            "dagger": {
-                "cost": 8,
-                "damage": 4,
-                "armor": 0,
-            },
-            "shortsword": {
-                "cost": 10,
-                "damage": 5,
-                "armor": 0,
-            },
-            "warhammer": {
-                "cost": 25,
-                "damage": 6,
-                "armor": 0,
-            },
-            "longsword": {
-                "cost": 40,
-                "damage": 7,
-                "armor": 0,
-            },
-            "greataxe": {
-                "cost": 74,
-                "damage": 8,
-                "armor": 0,
-            },
+            "dagger": {"cost": 8, "damage": 4, "armor": 0},
+            "shortsword": {"cost": 10, "damage": 5, "armor": 0},
+            "warhammer": {"cost": 25, "damage": 6, "armor": 0},
+            "longsword": {"cost": 40, "damage": 7, "armor": 0},
+            "greataxe": {"cost": 74, "damage": 8, "armor": 0},
         }
         armors = {
-            "none": {
-                "cost": 0,
-                "damage": 0,
-                "armor": 0,
-            },
-            "leather": {
-                "cost": 13,
-                "damage": 0,
-                "armor": 1,
-            },
-            "chainmail": {
-                "cost": 31,
-                "damage": 0,
-                "armor": 2,
-            },
-            "splintmail": {
-                "cost": 53,
-                "damage": 0,
-                "armor": 3,
-            },
-            "bandedmail": {
-                "cost": 75,
-                "damage": 0,
-                "armor": 4,
-            },
-            "platemail": {
-                "cost": 102,
-                "damage": 0,
-                "armor": 5,
-            },
+            "none": {"cost": 0, "damage": 0, "armor": 0},
+            "leather": {"cost": 13, "damage": 0, "armor": 1},
+            "chainmail": {"cost": 31, "damage": 0, "armor": 2},
+            "splintmail": {"cost": 53, "damage": 0, "armor": 3},
+            "bandedmail": {"cost": 75, "damage": 0, "armor": 4},
+            "platemail": {"cost": 102, "damage": 0, "armor": 5},
         }
         rings = {
-            "none1": {
-                "cost": 0,
-                "damage": 0,
-                "armor": 0,
-            },
-            "none2": {
-                "cost": 0,
-                "damage": 0,
-                "armor": 0,
-            },
-            "damage1": {
-                "cost": 25,
-                "damage": 1,
-                "armor": 0,
-            },
-            "damage2": {
-                "cost": 50,
-                "damage": 2,
-                "armor": 0,
-            },
-            "damage3": {
-                "cost": 100,
-                "damage": 3,
-                "armor": 0,
-            },
-            "defense1": {
-                "cost": 20,
-                "damage": 0,
-                "armor": 1,
-            },
-            "defense2": {
-                "cost": 40,
-                "damage": 0,
-                "armor": 2,
-            },
-            "defense3": {
-                "cost": 80,
-                "damage": 0,
-                "armor": 3,
-            },
+            "none1": {"cost": 0, "damage": 0, "armor": 0},
+            "none2": {"cost": 0, "damage": 0, "armor": 0},
+            "damage1": {"cost": 25, "damage": 1, "armor": 0},
+            "damage2": {"cost": 50, "damage": 2, "armor": 0},
+            "damage3": {"cost": 100, "damage": 3, "armor": 0},
+            "defense1": {"cost": 20, "damage": 0, "armor": 1},
+            "defense2": {"cost": 40, "damage": 0, "armor": 2},
+            "defense3": {"cost": 80, "damage": 0, "armor": 3},
         }
 
         rings_combinations = list(combinations(rings.values(), 2))
